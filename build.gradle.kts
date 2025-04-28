91afb1d1e3a3e8e8d7d9a79bd44290712f22cf4b
Ceyda Özarslan <80062366+ceyzarslan@users.noreply.github.com>
2025-04-28 14:01:30 +0200

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,13 +1,128 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
+
 plugins {
-    alias(libs.plugins.androidApplication) apply false
-    alias(libs.plugins.jetbrainsKotlinAndroid) apply false
-    id("com.google.dagger.hilt.android") version "2.44" apply false
-    id("com.google.gms.google-services") version "4.4.1" apply false
+    alias(libs.plugins.androidApplication)
+    alias(libs.plugins.jetbrainsKotlinAndroid)
+    kotlin("kapt")
+    id("com.google.dagger.hilt.android")
+    id("com.google.gms.google-services")
 }
 
-buildscript {
-    dependencies {
-        classpath("com.google.dagger:hilt-android-gradle-plugin:2.48")
+android {
+    namespace = "com.berkaykurtoglu.securevisage"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.berkaykurtoglu.securevisage"
+        minSdk = 24
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+        vectorDrawables {
+            useSupportLibrary = true
+        }
     }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        isCoreLibraryDesugaringEnabled = true
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        compose = true
+    }
+    composeOptions {
+        kotlinCompilerExtensionVersion = "1.5.1"
+    }
+    packaging {
+        resources {
+            excludes += "/META-INF/{AL2.0,LGPL2.1}"
+        }
+    }
+
+    // Allow references to generated code
+    kapt {
+        correctErrorTypes = true
+    }
+}
+
+dependencies {
+
+
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.lifecycle.runtime.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(platform(libs.androidx.compose.bom))
+    implementation(libs.androidx.ui)
+    implementation(libs.androidx.ui.graphics)
+    implementation(libs.androidx.ui.tooling.preview)
+    implementation(libs.androidx.material3)
+    testImplementation(libs.junit)
+    androidTestImplementation(libs.androidx.junit)
+    androidTestImplementation(libs.androidx.espresso.core)
+    androidTestImplementation(platform(libs.androidx.compose.bom))
+    androidTestImplementation(libs.androidx.ui.test.junit4)
+    debugImplementation(libs.androidx.ui.tooling)
+    debugImplementation(libs.androidx.ui.test.manifest)
+
+    //Compose Navigation
+    implementation(libs.androidx.navigation.compose)
+
+    // Amplify API and Datastore dependencies
+    implementation ("com.amplifyframework:core:2.16.1")
+    implementation (libs.aws.api)
+    implementation (libs.aws.datastore)
+    implementation (libs.aws.auth.cognito)
+    implementation (libs.aws.storage.s3)
+    implementation (libs.aws.push.notifications.pinpoint)
+    implementation (libs.aws.datastore.v216)
+
+    // Authenticator dependency
+    implementation (libs.authenticator)
+
+    // Support for Java 8 features
+    coreLibraryDesugaring (libs.desugar.jdk.libs)
+
+    //Coroutines
+    implementation(libs.kotlinx.coroutines.android)
+
+    //Material Icons
+    implementation ("androidx.compose.material:material-icons-extended")
+
+    /*//Dagger - Hilt
+    implementation ("com.google.dagger:hilt-android:2.44")
+    kapt ("com.google.dagger:hilt-android-compiler:2.44")
+    kapt ("androidx.hilt:hilt-compiler:1.0.0")
+    implementation ("androidx.hilt:hilt-navigation-compose:1.0.0")
+    implementation ("androidx.hilt:hilt-work:1.0.0")
+    implementation ("androidx.work:work-runtime-ktx:2.8.1")*/
+
+    implementation(libs.hilt.android)
+    kapt (libs.hilt.android.compiler)
+    implementation (libs.androidx.hilt.navigation.compose)
+
+    //Permission Accomphanist
+    implementation (libs.accompanist.permissions)
+
+    //Coil
+    implementation(libs.coil.compose)
+
+    //Firebase BOM
+    implementation(platform(libs.firebase.bom))
+
 }
